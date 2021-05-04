7715fa1076dec7511ceee786c6639b3bf0580911
Joan Pedro <jpos.senai@gmail.com>
2021-05-04 16:16:40 -0300

    feat: Using NativeElement vs DebugElement for Testing Html TEMPLATE

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { HeroComponent } from './hero.component';
 import { ComponentFixture, TestBed } from "@angular/core/testing"
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HeroComponent (Shallow Test)', () => {
   let fixture: ComponentFixture<HeroComponent>;
@@ -24,7 +25,7 @@ describe('HeroComponent (Shallow Test)', () => {
     expect(sut.hero.name).toEqual('Any Fake Hero')
   })
 
-  it('Should render the hero name in an anchor tag', () => {
+  it('Should render the hero name in an anchor tag (Using Native DOM)', () => {
     const { sut } = makeSut(fixture);
     sut.hero = { id: 1, name: 'Any Fake Hero', strength: 3 };
 
@@ -38,5 +39,21 @@ describe('HeroComponent (Shallow Test)', () => {
     const anchorContent = doc.querySelector('a').textContent;
 
     expect(anchorContent).toContain('Any Fake Hero')
+  })
+
+  it('Should render the hero name in an anchor tag (Using DebugElement Angular)', () => {
+    const { sut } = makeSut(fixture);
+    sut.hero = { id: 1, name: 'Any Fake Hero', strength: 3 };
+
+    // Enable trigger changes detection cycle for the component (Bindings/Interpolation)!
+    fixture.detectChanges();
+
+    // All entiry document HTML page with some Angular helper methods!
+    const deb = fixture.debugElement as DebugElement;
+
+    // Select anchor element!
+    const anchorElement = deb.query(By.css('a')).nativeElement as HTMLAnchorElement;
+
+    expect(anchorElement.textContent).toContain('Any Fake Hero')
   })
 })
