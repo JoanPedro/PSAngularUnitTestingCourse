68879e258e46846fb3675bab697298d4bb8a47cc
Joan Pedro <jpos.senai@gmail.com>
2021-05-05 09:59:35 -0300

    refactor: Move SUT creation to is own factory method

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -1,27 +1,24 @@
 import { StrengthPipe } from './strength.pipe';
 
 describe('Strength Pipe', () => {
+  let sut: StrengthPipe;
+
+  const makeSut = () => new StrengthPipe();
+
+  beforeEach(() => sut = makeSut())
+
   it('Should display weak if strenght is 5', () => {
-    const sut = new StrengthPipe();
-
     const result = sut.transform(5);
-
     expect(result).toEqual('5 (weak)');
   })
 
   it('Should display strong if strenght is 10', () => {
-    const sut = new StrengthPipe();
-
     const result = sut.transform(10);
-
     expect(result).toEqual('10 (strong)');
   })
 
   it('Should display unbelievable if strenght is 20', () => {
-    const sut = new StrengthPipe();
-
     const result = sut.transform(20);
-
     expect(result).toEqual('20 (unbelievable)');
   })
 })
