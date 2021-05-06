7e04bd02c4ce9d911d86922a711442fc348b145d
Joan Pedro <jpos.senai@gmail.com>
2021-05-06 10:39:58 -0300

    feat: Adds test that dealing with NgModel Directive

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,9 +1,12 @@
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { Provider } from '@angular/core';
 import { Location } from '@angular/common';
 import { HeroService } from './../hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('HeroDetailComponent', () => {
   let mockHeroService: jasmine.SpyObj<HeroService>;
@@ -24,6 +27,7 @@ describe('HeroDetailComponent', () => {
     mockLocation = jasmine.createSpyObj(['back']);
 
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [HeroDetailComponent],
       providers: [
         { provide: HeroService, useValue: mockHeroService },
@@ -32,5 +36,18 @@ describe('HeroDetailComponent', () => {
       ] as Array<Provider>
     })
     fixture = TestBed.createComponent(HeroDetailComponent);
+    mockHeroService.getHero.and.returnValues(of({ id: 3, name: 'Any Fake Name', strength: 11 }))
+  })
+
+  it('Should render the hero name in a h2 tag', () => {
+    fixture.detectChanges();
+    const doc = fixture.nativeElement as HTMLDocument;
+    const h2Content = (doc.querySelector('h2') as HTMLHeadElement).textContent;
+
+    const debugElement = fixture.debugElement;
+    const h2Content_ = (debugElement.query(By.css('h2')).nativeElement as HTMLHeadElement).textContent;
+
+    expect(h2Content).toContain('ANY FAKE NAME')
+    expect(h2Content_).toContain('ANY FAKE NAME')
   })
 })
